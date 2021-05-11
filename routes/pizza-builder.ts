5ab758666982ae0e368596c06eac82ba9475121e
Yitz <yhochstadt@gmail.com>
2021-05-11 09:21:30 -0400

    math working

--- routes/pizza-builder.ts
+++ routes/pizza-builder.ts
@@ -9,20 +9,27 @@ routes.get("/pizza-builder", (req, res) => {
     res.render("pizza-builder", {toppings});
 });
 
+
 routes.get("/built-pizza", (req, res) => {
     const size = req.query.size as string;
-    const numberOfToppings:number = parseInt(req.query.numberOfToppings as string);
+    const toppingsNum:number = parseInt(req.query.toppingsNum as string);
     const glutenFree:boolean = !!req.query.glutenFree;
     const instructions = req.query.instructions;
+    console.log(size);
+    console.log(toppingsNum);
+    console.log(glutenFree);
+    console.log(instructions);
     let price = 0;
-    if (size === "small"){
-        price += 7 + (numberOfToppings * .50);
-    } else if (size === "medium"){
-        price += 10 + (numberOfToppings * 1);
+    if (size === "Small"){
+        price = 7 + (toppingsNum * .5);
+    } else if (size === "Medium"){
+        price = 10 + (toppingsNum * 1);
     } else {
-        price += 12 + (numberOfToppings * 1.25);
+        price = 12 + (toppingsNum * 1.25);
     }
-    res.render("built-pizza", {size, numberOfToppings, glutenFree, price, instructions});
+    let formattedPrice = price.toFixed(2);
+    res.render("built-pizza", {size, toppingsNum, glutenFree, formattedPrice, instructions});
 });
+
 
 export default routes;
